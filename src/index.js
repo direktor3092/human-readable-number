3cefbde0f0ea9f9a1367c8332c676cba8104928d
direktor3092 <direktor3092@gmail.com>
2023-09-12 13:31:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
+
+    const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  const str = number.toString();
+  const dozen = +str[1];
+  const n = +str[2];
+
+  if (number < 20) {
+    return numbers[number];
+  } else if (number < 100 && number % 10 !== 0) {
+    return `${dozens[Math.floor(number / 10) - 1]} ${numbers[number % 10]}`
+  } else if (number < 100 && number % 10 === 0) {
+    return `${dozens[Math.floor(number / 10) - 1]}`
+  } else if (number < 1000 && dozen !== 0 && dozen < 2) {
+    return `${numbers[Math.floor(number / 100)]} hundred ${numbers[number % 100]}`;
+  } else if (number < 1000 && dozen !== 0 && dozen > 1 && n === 0) {
+    return `${numbers[Math.floor(number / 100)]} hundred ${dozens[dozen - 1]}`;
+  }
+  else if (number < 1000 && dozen !== 0 && dozen > 1) {
+    return `${numbers[Math.floor(number / 100)]} hundred ${dozens[dozen - 1]} ${numbers[n]}`;
+  } else if (number < 1000 && dozen === 0 && n !== 0) {
+    return `${numbers[Math.floor(number / 100)]} hundred ${numbers[n]}`;
+  } else if (number < 1000 && dozen === 0 && n === 0) {
+    return `${numbers[Math.floor(number / 100)]} hundred`;
+  }
 }
+
